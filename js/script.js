fd1c5ddeaff208fcaaacbb3af027abc1bcd4119a
chandan-mandi <itscoderchandan@gmail.com>
2021-09-02 16:32:42 +0530

    at the end korai fellam

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 const bookContainer = document.getElementById('book-container');
 const bookFound = document.getElementById('total-book-found');
 const erorDiv = document.getElementById('eror-div');
+const spinner = document.getElementById("spinner");
 
 searchBtn.addEventListener('click', function() {
     const searchText = searchInput.value;
@@ -14,10 +15,21 @@ searchBtn.addEventListener('click', function() {
     // clear dom
     bookContainer.innerHTML = "";
     // data fetching
-    const url =`http://openlibrary.org/search.json?q=${searchText}`;
+    const url =`https://openlibrary.org/search.json?q=${searchText}`;
+    spinner.classList.remove("d-none");
     fetch(url)
     .then(res => res.json())
-    .then(data => showData(data))
+    .then((data) => {
+        // Setting a timer of 1.5s, before removing the spinnner, and showing data
+        setTimeout(() => {
+          spinner.classList.add("d-none");
+          showData(data);
+        }, 1500);
+      })
+      .finally(() => {
+        searchInput.value = "";
+      });
+    // searchInput.value = '';
 })
 const showData = bookArr => {
     
@@ -41,23 +53,26 @@ const showData = bookArr => {
         </div>
     `
     bookArray.forEach((item) =>{
-
         const div = document.createElement("div");
-        div.classList.add("col-md-4");
+        div.classList.add("col");
         div.innerHTML = `
-        <div class="">
-            <img class="w-50" src="https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg">
-            <h4>Book Name : ${item.title}</h4>
-            <p>Author Name : ${item.author_name}</p>
-            <p>Publisher Name : ${item.publisher}</p>
-            <p>First Publish : ${item.first_publish_year}</p>
+        <div class="card h-100">
+          <img src="https://covers.openlibrary.org/b/id/${item.cover_i}-L.jpg" class="card-img-top book-cover" alt="...">
+          <div class="card-body">
+            <h6 class="card-title">Name: <span class="text-primary fs-6">${item.title}</span></h6>
+            <h6 class="card-text">Author: <span class="text-primary fs-6">${item.author_name}</span></h6>
+            <h6 class="card-text">Publisher: <span class="text-primary fs-6">${item.publisher}</span></h6>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">First Publish : ${item.first_publish_year}</small>
+          </div>
         </div>
-         `;
+        `
          bookContainer.appendChild(div);
 
     })
 }
-
+// Total Book Counting 
 const showTotalBook = totalBook => {
     const foundBook = totalBook.numFound;
     console.log(foundBook)
